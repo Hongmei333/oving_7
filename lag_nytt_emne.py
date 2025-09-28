91581e374d2bf6e050190bfe37ea67f2559a90af
Hongmei333 <219445@uis.no>
2025-09-28 08:13:12 +0200

    Update lag_nytt_emne.py

--- lag_nytt_emne.py
+++ lag_nytt_emne.py
@@ -2,11 +2,15 @@ emnekode=[]
 sesong=[]
 studiepoeng=[]
 
-def lag_nytt_emne(kode,tid,poeng):
+def lag_nytt_emne():
+    kode=input("Skriv inn emnekode:")
+    tid=input("Skriv inn undervisningstid (høst/vår):")     
+    poeng=int(input("Skriv inn studieppoeng for emnet:"))
     emnekode.append(kode.upper())
     sesong.append(tid.capitalize())
     studiepoeng.append(poeng)
 
-lag_nytt_emne("dat100", 'høst', 10)
+lag_nytt_emne()
 
-print(f"Nytt emne:{emnekode[-1]}, {sesong[-1]}, {studiepoeng[-1]} poeng")
+
+print(f"Nytt emne: {emnekode[-1]}, {sesong[-1]}, {studiepoeng[-1]} poeng")
